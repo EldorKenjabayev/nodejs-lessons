70e431e6c5b212cf62c48decf7c6ebf8e793c0e1
Eldor Kenjebayev <eldorkenjebayev@MacBook-Air-Eldor.local>
2025-03-07 03:29:38 +0500

    module import

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const fs = require('fs')
-fs.readFile('some.txt', 'utf-8', (err, data)=>{
-    fs.writeFile('some.txt', data + '\nSalom dunyo', (err, data)=>{
-        console.log('vse srabotalo');
+const math_lib = require('./my_math')
+
+let result_plus = math_lib.add(4, 5);
+console.log(result_plus);
+
+
+// const fs = require('fs')
+// fs.readFile('some.txt', 'utf-8', (err, data)=>{
+//     fs.writeFile('some.txt', data + '\nSalom dunyo', (err, data)=>{
+//         console.log('vse srabotalo');
         
-    })
-});
+//     })
+// });
