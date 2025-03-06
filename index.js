e00f0bbd341b014b9b0f65ac1e7f9047ce513709
Eldor Kenjebayev <eldorkenjebayev@MacBook-Air-Eldor.local>
2025-03-07 03:50:14 +0500

    create directory and delete directory

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const math_lib = require('./my_math')
+// const math_lib = require('./my_math')
 
-let result_plus = math_lib.add(4, 5);
-console.log(result_plus);
+// let result_plus = math_lib.add(4, 5);
+// console.log(result_plus);
 
 
 // const fs = require('fs')
@@ -11,3 +11,19 @@ console.log(result_plus);
         
 //     })
 // });
+
+// const fs = require('fs');
+
+// fs.mkdir('text-files', ()=>{
+//     fs.writeFile('./text-files/some.txt', 'hello',()=>{});
+// })
+
+
+const fs = require('fs');
+
+fs.unlink('./text-files/some.txt', ()=>{
+    fs.rmdir('./text-files', ()=>{
+        console.log('Delete succes');
+        
+    })
+})
